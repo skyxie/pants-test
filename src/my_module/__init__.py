4588544869b366c6636b090d569cb7037eec7644
Tian <tian.xie@enigma.com>
2022-11-30 23:13:17 -0500

    Introduce failing case

--- src/my_module/__init__.py
+++ src/my_module/__init__.py
@@ -1,5 +1,6 @@
+import tldextract
 from pyspark.sql.functions import udf
-from pyspark.sql.types import IntegerType
+from pyspark.sql.types import IntegerType, StringType
 
 __version__ = "0.1.0"
 __author__ = "Tian Xie <tian.xie@enigma.com>"
@@ -8,3 +9,8 @@ __author__ = "Tian Xie <tian.xie@enigma.com>"
 @udf(returnType=IntegerType())
 def times_two(base):
     return base * 2
+
+
+@udf(returnType=StringType())
+def get_domain(url):
+    return tldextract.extract(url).domain
