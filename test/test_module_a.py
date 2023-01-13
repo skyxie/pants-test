e4b6975fd321f9e2ec15e452b7b45f17105e7504
Tian <tian.xie@enigma.com>
2023-01-12 22:49:31 -0500

    Remove multiple resolves

--- test/test_module_a.py
+++ test/test_module_a.py
@@ -1,9 +1,9 @@
 import pytest
 
-from module_a import get_domain
+from module_a import get_domain, plus_one
 
 
-@pytest.mark.parameterize(
+@pytest.mark.parametrize(
     "url,expected_domain",
     [
         ("https://google.com", "google"),
@@ -13,3 +13,10 @@ from module_a import get_domain
 )
 def test_get_domain(url, expected_domain):
     assert get_domain(url) == expected_domain
+
+
+@pytest.mark.parametrize(
+    "x,y", [(1, 2), (-1, 0)]
+)
+def test_plus_one(x, y):
+    assert plus_one(x) == y
