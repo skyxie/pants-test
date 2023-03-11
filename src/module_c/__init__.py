5132a224aa4181222f1c7484535dc9b4e5f70e03
Tian <tian.xie@enigma.com>
2023-03-10 21:49:48 -0500

    Create pants cannot infer owners error

--- src/module_c/__init__.py
+++ src/module_c/__init__.py
@@ -1,5 +1,11 @@
+import requests
+
 from module_b import plus_y
 
 
 def times_two(x: int) -> int:
     return plus_y(x, x)
+
+
+def get(url: str) -> int:
+    return requests.get(url).status_code
--- src/module_d/__init__.py
+++ src/module_d/__init__.py
@@ -1,5 +1,11 @@
+import requests
+
 from module_a import plus_one
 
 
 def plus_two(x: int) -> int:
     return plus_one(plus_one(x))
+
+
+def get(url: str) -> int:
+    return requests.get(url).status_code
